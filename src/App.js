addec556e123d62ac8371c1b7b4cb159b1584fe3
Alex Oh <odange2119@gmail.com>
2025-04-01 00:23:52 +0900

    first commit44

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Box from "./component/Box"
+
+//복습하기
+// 1. 박스 2개 (타이틀, 사진, 결과)
+// 2. 가위, 바위, 보 버튼이 있다.
+// 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
+// 4. 컴퓨터는 랜덤하게 아이템 선택이 된다.
+// 5. 3, 4번의 결과를 가지고 누가 이겼는지 승패를 따진다.
+// 6. 승패 결과에 따라 테두리 색이 바뀐다. (이기면 - 초록, 지면 - 빨강, 비기면 - 검정색)
+
+const choice = {
+  rock:{
+    name:"Rock",
+    img:"https://scienceresourcebox.co.nz/cdn/shop/files/Chalkrounded_WEB.jpg?v=1684441843"
+  },
+  scissors:{
+    name:"Scissors",
+    img:"https://m.media-amazon.com/images/I/51VWwCMeZOL._AC_UF894,1000_QL80_.jpg"
+  },
+  paper:{
+    name:"Paper",
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuXW8UgLnh5BGDf6ZcV32KHs5QF7CLMxau2w&s",
+  },
+};
 
 function App() {
+  let [userSelect, setUserSelect] = useState(null)
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='main'>
+        <Box title="you" item={userSelect}/>
+        {/* <Box title="computer"/> */}
+      </div>
+      <div className='main'>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
     </div>
   );
 }
